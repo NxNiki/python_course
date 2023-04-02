360b7da5bde0b41957502e8efff0954b069d088c
NxNiki <michaelniki1988@gmail.com>
2023-04-01 21:06:08 -0400

    6_exceptions

---
+++ 6_exceptions.py
@@ -0,0 +1,40 @@
+from timeit import timeit
+
+code1 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age cannot be 0 or less.")
+    return 10 / age
+
+
+try:
+    calculate_xfactor(-1)
+except ValueError as err:
+    print(err)
+"""
+
+code2 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age cannot be 0 or less.")
+    return 10 / age
+
+
+try:
+    calculate_xfactor(-1)
+except ValueError as err:
+    pass
+"""
+
+code3 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        return None
+    return 10 / age
+
+
+calculate_xfactor(-1)
+    pass
+"""
+print("first code=", timeit(code1, number=10000))
+print("second code=", timeit(code2, number=10000))
