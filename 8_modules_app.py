9b5849fe40562c7fa725e7bb41a43aa5a5a406f5
Xin Niu <michaelniki1988@gmail.com>
2023-08-18 19:00:34 -0400

    Update 8_modules_app.py

--- 8_modules_app.py
+++ 8_modules_app.py
@@ -13,5 +13,27 @@ import sys
 import(sys.path)
 
 # import modules in a subdirectory (packages)
+# A package is a container for one or more modules.
+# A package is mapped to a directory and a module is mapped to a file
 
+import ecommerce.sales
+from ecommerce.sales import calc_tax
+
+# sub-packages:
+# shopping is a sub-directory under ecommerce
+import ecommerce.shopping.sales
+
+# intra-package reference:
+
+from ecommerce.customer import contract
+from ..customer import contact
+
+# show all the attributes and methods to find in an object.
+print(dir(sales))
+
+print(sales.__name__)
+print(sales.__package__)
+print(sales.__file__)
+
+# execute modules as scripts:
 
