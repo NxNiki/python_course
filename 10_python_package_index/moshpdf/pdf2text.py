d2ea55adc5983ed7002ad94a3505c2c078f8480b
Xin Niu <michaelniki1988@gmail.com>
2023-08-24 17:12:52 -0400

    Update pdf2text.py

--- 10_python_package_index/moshpdf/pdf2text.py
+++ 10_python_package_index/moshpdf/pdf2text.py
@@ -1,1 +1,20 @@
-print("pdf2text")
+""" One line description. """
+
+""" One ine description.
+
+    A more detailed explanation.
+"""
+
+""" This module provides functions to convert a PDF to test" """
+
+def convert(path):
+  """ 
+  Convert the given PDF to text.
+
+  Parameters: 
+  path (str): The path to a PDF file.
+
+  Returns:
+  str: The content of the PDF file as text.
+  """
+  print("pdf2text")
