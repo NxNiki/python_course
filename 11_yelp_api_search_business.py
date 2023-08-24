fa46d6c0c52cac70dd6c22743caeb8ad807c1f7e
Xin Niu <michaelniki1988@gmail.com>
2023-08-24 17:43:22 -0400

    Create 11_yelp_api_search_business.py

---
+++ 11_yelp_api_search_business.py
@@ -0,0 +1,23 @@
+# set up account for yelp api before running this.
+
+import requests
+url = "https://api.yelp.com/v3/businesses/search"
+api_key = "" # set the api key on yelp api
+headers = {
+  "Authorization": "Bearer " + api_key
+}
+params = {
+  # "term": "Barber", 
+  "loacation": "NYC"
+}
+
+response = requests.get(url, headers=headers, params=params)
+print(response.text)
+print(response.json())
+
+businesses = response.json()["businesses"]
+
+for business in businesses:
+  print(business["name"])
+
+names = [business["name"] for business in businesses if business["rating"] > 4.5]
